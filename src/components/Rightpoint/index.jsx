f66529df236fbc93751406c681661e5810b60b52
pyroplushy <92647431+pyroplushy@users.noreply.github.com>
2023-09-14 19:53:04 -0400

    rewrote Rightpoint copy

--- src/components/Rightpoint/index.jsx
+++ src/components/Rightpoint/index.jsx
@@ -12,36 +12,36 @@ export const Rightpoint = () => {
       <div className="Rightpoint_TextBlock">
         <h2 className="Rightpoint_Title">Rightpoint</h2>
         <h2 className="Rightpoint_Summary">
-          I really enjoyed my time at Righpoint because I was able to grow a lot
-          as a developer in a short time by working for many different teams and
-          clients.
+          I really enjoyed my time as a consultant at Righpoint because I was
+          able to grow a lot as a developer in a short time by working with many
+          different teams and for many different clients.
         </h2>
         {hideParagraph ? (
           ""
         ) : (
           <p className="Rightpoint_Paragraph">
-            I started as a coop under Tandemseven, a Genpact company. While I
-            was at Tandemseven Genpact, acquired Rightpoint and merged us into
-            the company. I was offered a full time salaried position in february
-            2020. I worked there until I was laid off in march 2023. While at
-            Rightpoint I had the opportunity to work with many different
-            companies in many different industries. Airlines, banks, pharmacies,
-            and many others. My work was largely based in React.js though I did
-            work on an angular project for a few months. I was brought onto
-            projects to serve different purposes. When I started as a lower
-            level developer I was valuable to companies because I could create
-            components and fix bugs with little supervision. As I developed in
-            my career I was brought on to do more foundational set up and
-            discovery work. I also acted as what I like to call a pinch hitter.
-            There were projects that needed a developer to jump in and start
-            delivering and I was happy to help. This sometimes meant working
-            late hours and pivoting between tasks quickly. I also got to see
-            many different team structures and variations of the agile process.
-            I was able to work with clients to try to improve their existing
-            processes for the benefit of all involved.
+            While at Rightpoint I worked with clients in industries such as:
+            Airlines, banking, pharmaceutical, and more. My work was largely
+            based in React with some time spent on angular.
+            <br /> As a Frontend developer I was brought onto projects to serve
+            different purposes. When I started as a lower level developer I was
+            valuable to companies because I could create components and fix bugs
+            with little supervision. As I developed in my career I was brought
+            on to do more foundational set up and discovery work. I also acted
+            as a pinch hitter for the company. There were projects that needed a
+            developer to jump in and start delivering and I jumped at the
+            chance. This sometimes meant working late hours and pivoting between
+            tasks quickly. <br />
+            One of the benefits of consulting is being able to see different
+            team structures and variations of the agile process in addition to
+            the different front end technologies each project used.
+            <br /> I worked at Rightpoint until I was laid off in march 2023. I
+            really enjoyed my time at the company and I'm very grateful for the
+            chance I was given to learn from so many great developers,
+            designers, and project managers.
           </p>
         )}
-        <button className="RIT_ReadMore" onClick={readMoreOnClick}>
+        <button className="Rightpoint_ReadMore" onClick={readMoreOnClick}>
           {hideParagraph ? "Read more" : "Hide"}
         </button>
       </div>
