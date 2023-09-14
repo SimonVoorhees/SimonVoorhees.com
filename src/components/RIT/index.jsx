bde29aa91241f00fedd7a309be32a3db6a15a95e
pyroplushy <92647431+pyroplushy@users.noreply.github.com>
2023-09-14 12:57:10 -0400

    update mobile styles, readd CNAME file

--- src/components/RIT/index.jsx
+++ src/components/RIT/index.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import RITLogo from "../../assets/RIT_rgb_vert_k.jpg";
 
 export const RIT = () => {
+  const [hideParagraph, setHideParagraph] = useState(true);
+  const readMoreOnClick = () => {
+    setHideParagraph(!hideParagraph);
+  };
   return (
     <section className="RIT">
       <img
@@ -11,31 +15,39 @@ export const RIT = () => {
         alt="Rochester Institute of Technology logo"
       />
       <div className="RIT_TextBlock">
-        <p className="RIT_Paragraph">
-          <h2 className="RIT_Summary">
-            I attended RIT from 2014-2018 before taking some time off to get a
-            coop. I finished my degree in Game Design and Development and
-            graduated in 2020
-          </h2>
-          I attended RIT as a Game Design and Development(GDD) major. There's a
-          lot of overlap between GDD courses and another major, New Media. New
-          Media is a web development major, so part of my course requirements
-          were some New Media classes. I wasn't a big fan of this at first, but
-          thanks to some inspring and fun teachers I grew to love web development and these classes. At the
-          same time I started to like web dev I fell out of love with game dev.
-          Games are fun and exciting, but they also require a lot of moving
-          parts. Organizing with other students to get art done and having
-          disagreements over design concepts left me disillusioned with the
-          process. I learned that I was too close to games to be able to work
-          well with a team. I had strong opinions about each project I worked on
-          and felt like I wouldn't be happy with the final product. This was a
-          self fulfilling prophecy. Web dev on the other hand was a joy to work
-          on. It felt great arranging everything just so, and I was really
-          interested in learning about a11y. It was easy for me to stay up to
-          date on things like semantic html because I liked learning about them.
-          I decided that Game development would be a hobby and web dev would be
-          a career
-        </p>
+      <h2 className="RIT_Title">RIT</h2>
+        <h2 className="RIT_Summary">
+          I attended RIT from 2014-2018 before taking some time off to get a
+          coop. I finished my degree in Game Design and Development and
+          graduated in 2020
+        </h2>
+        {hideParagraph ? (
+          ""
+        ) : (
+          <p className="RIT_Paragraph">
+            I attended RIT as a Game Design and Development(GDD) major. There's
+            a lot of overlap between GDD courses and another major, New Media.
+            New Media is a web development major, so part of my course
+            requirements were some New Media classes. I wasn't a big fan of this
+            at first, but thanks to some inspring and fun teachers I grew to
+            love web development and these classes. At the same time I started
+            to like web dev I fell out of love with game dev. Games are fun and
+            exciting, but they also require a lot of moving parts. Organizing
+            with other students to get art done and having disagreements over
+            design concepts left me disillusioned with the process. I learned
+            that I was too close to games to be able to work well with a team. I
+            had strong opinions about each project I worked on and felt like I
+            wouldn't be happy with the final product. This was a self fulfilling
+            prophecy. Web dev on the other hand was a joy to work on. It felt
+            great arranging everything just so, and I was really interested in
+            learning about a11y. It was easy for me to stay up to date on things
+            like semantic html because I liked learning about them. I decided
+            that Game development would be a hobby and web dev would be a career
+          </p>
+        )}
+        <button className="RIT_ReadMore" onClick={readMoreOnClick}>
+          {hideParagraph ? "Read more" : "Hide"}
+        </button>
       </div>
     </section>
   );
--- src/components/Rightpoint/index.jsx
+++ src/components/Rightpoint/index.jsx
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import RightpointLogo from "../../assets/rightpoint_logo.jpg";
 
 export const Rightpoint = () => {
+  const [hideParagraph, setHideParagraph] = useState(true);
+  const readMoreOnClick = () => {
+    setHideParagraph(!hideParagraph);
+  };
   return (
     <section className="Rightpoint">
       <div className="Rightpoint_TextBlock">
+        <h2 className="Rightpoint_Title">Rightpoint</h2>
         <h2 className="Rightpoint_Summary">
-          I really enjoyed my time at Righpoint because I was able to grow a lot as a developer in a short time by working for many different teams and clients.
+          I really enjoyed my time at Righpoint because I was able to grow a lot
+          as a developer in a short time by working for many different teams and
+          clients.
         </h2>
-        <p className="Rightpoint_Paragraph">
-          I started as a coop under Tandemseven, a Genpact company. While I was
-          at Tandemseven Genpact, acquired Rightpoint and merged us into the
-          company. I was offered a full time salaried position in february 2020.
-          I worked there until I was laid off in march 2023. While at Rightpoint
-          I had the opportunity to work with many different companies in many
-          different industries. Airlines, banks, pharmacies, and many others. My
-          work was largely based in React.js though I did work on an angular
-          project for a few months. I was brought onto projects to serve
-          different purposes. When I started as a lower level developer I was
-          valuable to companies because I could create components and fix bugs
-          with little supervision. As I developed in my career I was brought on
-          to do more foundational set up and discovery work. I also acted as
-          what I like to call a pinch hitter. There were projects that needed a
-          developer to jump in and start delivering and I was happy to help.
-          This sometimes meant working late hours and pivoting between tasks
-          quickly. I also got to see many different team structures and
-          variations of the agile process. I was able to work with clients to
-          try to improve their existing processes for the benefit of all
-          involved.
-        </p>
+        {hideParagraph ? (
+          ""
+        ) : (
+          <p className="Rightpoint_Paragraph">
+            I started as a coop under Tandemseven, a Genpact company. While I
+            was at Tandemseven Genpact, acquired Rightpoint and merged us into
+            the company. I was offered a full time salaried position in february
+            2020. I worked there until I was laid off in march 2023. While at
+            Rightpoint I had the opportunity to work with many different
+            companies in many different industries. Airlines, banks, pharmacies,
+            and many others. My work was largely based in React.js though I did
+            work on an angular project for a few months. I was brought onto
+            projects to serve different purposes. When I started as a lower
+            level developer I was valuable to companies because I could create
+            components and fix bugs with little supervision. As I developed in
+            my career I was brought on to do more foundational set up and
+            discovery work. I also acted as what I like to call a pinch hitter.
+            There were projects that needed a developer to jump in and start
+            delivering and I was happy to help. This sometimes meant working
+            late hours and pivoting between tasks quickly. I also got to see
+            many different team structures and variations of the agile process.
+            I was able to work with clients to try to improve their existing
+            processes for the benefit of all involved.
+          </p>
+        )}
+        <button className="RIT_ReadMore" onClick={readMoreOnClick}>
+          {hideParagraph ? "Read more" : "Hide"}
+        </button>
       </div>
       <img
         className="Rightpoint_Logo"
