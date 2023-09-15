5d9b746132a013346021a4c8844077f1e5b0579d
pyroplushy <92647431+pyroplushy@users.noreply.github.com>
2023-09-14 22:05:06 -0400

    added project descriptions, spacing fixes

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -6,11 +6,43 @@ export const Projects = () => {
     <section className="Projects">
       <div className="Projects_TextBlock">
         <h2 className="Projects_Title">Projects</h2>
-        <p className="Projects_Paragraph">These are some of the projects I've made over the years. Most of my work at Rightpoint is not public knowledge, so my portfolio is currently the projects that are public knowledge and personal projects I've made over the years</p>
+        <h2 className="Projects_Paragraph">
+          These are some of the projects I've made over the years. Most of my
+          work at Rightpoint is not public knowledge, so my portfolio is
+          currently the projects that are public knowledge and personal projects
+          I've made over the years
+        </h2>
         <ul className="Projects_Links">
-        <li><a className="Projects_Link" target="_blank"href="https://www.rightpoint.com/work/genpact-oasis">Genpact Oasis</a></li>
-        <li><a className="Projects_Link" target="_blank"href="https://simonvoorhees.github.io/SpaceX-Launches-App/">SpaceX Launches</a></li>
-        <li><a className="Projects_Link" target="_blank"href="https://simonvoorhees.github.io/ReactDashBoard/">React Dashboard</a></li>
+          <li>
+            <a
+              className="Projects_Link"
+              target="_blank"
+              href="https://www.rightpoint.com/work/genpact-oasis"
+            >
+              Genpact Oasis
+            </a>
+            <p>A project I worked on at Rightpoint. I worked directly with a small team to quickly prototype the virtual environment then scale it up to the full customizable experience.</p>
+          </li>
+          <li>
+            <a
+              className="Projects_Link"
+              target="_blank"
+              href="https://simonvoorhees.github.io/SpaceX-Launches-App/"
+            >
+              SpaceX Launches
+            </a>
+            <p>This is a simple project that showcases some of my more technical skills. I make calls to the SpaceX API and display data from that. I also included a search function and some basic unit tests.</p>
+          </li>
+          <li>
+            <a
+              className="Projects_Link"
+              target="_blank"
+              href="https://simonvoorhees.github.io/ReactDashBoard/"
+            >
+              React Dashboard
+            </a>
+            <p>This project is purely about visuals. The goal was to take a wireframe dashboard and display it with dummy data and images. There isn't much functionality other than the to do list.</p>
+          </li>
         </ul>
       </div>
     </section>
